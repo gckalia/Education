20954a42817cc80021d20793a1724a75c51f9075
gckalia <girishchander.kalia@gmail.com>
2018-01-05 10:52:44 +0000

    corrected the temp array size

--- Langauge/C-Language/string_reverse.c
+++ Langauge/C-Language/string_reverse.c
@@ -1,20 +1,23 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <string.h>
 
-void FirstReverse(char * str) { 
+void FirstReverse(char * str ) { 
   
   // code goes here  
   int len = strlen(str);
   int i;
-  char res[len];
+  char res[len+1];
   
   for(i=0;i<len;i++)
   {
       res[i] = str[len-1-i];
   }
+  res[i] = 0;
   
   printf("%s", res); 
             
 }
+
 
 int main(void) { 
 
@@ -25,4 +28,4 @@ int main(void) {
   FirstReverse(gets(stdin));
   return 0;
     
-} 
+}
